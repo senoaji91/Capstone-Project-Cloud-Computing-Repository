7d1d9e5059513174fbe29949b88785bc8ec6669a
Seno Aji Wicaksono <seno.aji91@ui.ac.id>
2022-06-12 20:54:38 +0700

    production rev1.26 revisi python code

---
+++ python-1.py
@@ -0,0 +1,25 @@
+from matplotlib.pyplot import axis
+import requests
+import json
+import numpy as np
+import cv2
+import tensorflow as tf
+import sys
+
+image=cv2.imread(sys.argv[1])
+image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+image=cv2.resize(image,(180,180))/255
+image=np.expand_dims(image,axis=0)
+kitab={"0":"Acne","1":'Eksim',"2":"Normal","3":"Rosacea"}
+url='http://34.101.232.105:8081/v1/models/skut_testing:predict'
+data=json.dumps({'signature_name':"serving_default","instances":image.tolist()})
+headers={'content-type':'application/json'}
+response=requests.post(url,data=data,headers=headers)
+prediction=json.loads(response.text)['predictions']
+prediction=prediction[0]
+result={}
+for i in range(len(prediction)):
+     result[kitab.get(str(i))]=prediction[i]
+# with open("prediction.json", "w") as outfile:
+#     json.dump(result, outfile)
+sys.stdout.write(json.dumps(result))
